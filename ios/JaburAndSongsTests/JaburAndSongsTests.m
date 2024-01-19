4b7ec533473526324fa779b4545c2fc0771140d6
Andrewroy3535 <eda.twinkle@gmail.com>
2024-01-19 19:00:32 +0600

    rename to JaburAndSongs

--- ios/JaburAndSongsTests/JaburAndSongsTests.m
+++ ios/JaburAndSongsTests/JaburAndSongsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface JaburAndSongsTests : XCTestCase
+@interface jaburandsongsTests : XCTestCase
 
 @end
 
-@implementation JaburAndSongsTests
+@implementation jaburandsongsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
